7c88fd5a01448be8c194ec6218499ec92105b76e
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2016-03-10 10:57:27 -0500

    fixing alignment

--- src/frontend/open_source/pub/projects/list.cljs
+++ src/frontend/open_source/pub/projects/list.cljs
@@ -23,14 +23,14 @@
             tags @tags]
         (println listings)
         [:div
+         [:div.intro
+          [:div.main
+           [:p "Looking to improve your skills and contribute? These projects are active and welcome contributors."]]
+          [:div.secondary
+           [:button.submit.target
+            {:on-click #(r/nav "/projects/new" "Post your project")}
+            "Post Your Project"]]]
          [:div.main.listings.public
-          [:div.nav.clearfix
-           [:div.search
-            [:input {:type :search
-                     :placeholder "Search: `music`, `database` ..."
-                     :on-change #(fh/handle-change % [:forms :projects :search] :query)
-                     :value @query}]]]
-          [:p "Looking to improve your skills and contribute? These projects are active and welcome beginners."]
           [:div.listing-list
            (for [l listings]
              ^{:key (str "os-project-" (:slug l))}
@@ -39,18 +39,21 @@
                [:div.core
                 [:div.title [ui/attr l :project/name]]
                 [ui/attr l :project/tagline]
+                (if (:project/beginner-friendly l)
+                  [:div.beginner-friendly "beginner friendly"])
                 (if-let [t (:record/tags l)]
                   [:div.tags
                    (for [tag (map str/trim (str/split t ","))]
                      ^{:key (gensym)} [filter-tag tag])])]]])]]
          [:div.secondary.listings
-          [:div.section
-           [:div
-            [:button.submit.target
-             {:on-click #(r/nav "/projects/new" "Post your project")}
-             "Post your project"]]]
+          [:div.section.search
+           [:input {:type :search
+                    :placeholder "Search: `music`, `database` ..."
+                    :on-change #(fh/handle-change % [:forms :projects :search] :query)
+                    :value @query}]]
+          [:div.section.beginner-toggle
+           ]
           [:div.section.tags
-           [:h3 "Filter by Tag"]
            [:div
             (for [tag tags]
               ^{:key (gensym)} [filter-tag tag])]]
