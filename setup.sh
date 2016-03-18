d1ff67816b48abf169fe0ebf6a75b93348a9ab0d
Hristo Yankov <ico.yankov@gmail.com>
2016-03-18 09:31:11 +0200

    Add bower install to setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-add-apt-repository ppa:openjdk-r/ppa
-apt-get -y update
-apt-get install -y openjdk-8-jdk
+# add-apt-repository ppa:openjdk-r/ppa
+# apt-get -y update
+# apt-get install -y openjdk-8-jdk
 
-apt-get install -y nodejs-legacy
-apt-get install -y npm
-npm install -g bower
-bower install /vagrant/resources/sass/bower.json
+# curl -sL https://deb.nodesource.com/setup | bash -
+# apt-get install -y nodejs
+
+# apt-get install -y git
+
+# npm install npm -g
+
+# npm cache clean -f > /dev/null 2>&1
+# npm install -g n
+# n stable > /dev/null 2>&1
+
+# npm install -g bower
+
+su vagrant -c "cd /vagrant/resources/sass && bower install bower.json"
+
+exit 0
